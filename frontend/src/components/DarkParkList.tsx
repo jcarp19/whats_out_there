6487ceb9f5f008cc5d208c7c1ea5bbd3b546bdb0
rachel-rebecca <Rarebecc@umich.edu>
2021-10-28 16:16:03 -0400

    added empty component files to folder with comments describing what contents should be and added a models folder with an interface for one dark park

---
+++ frontend/src/components/DarkParkList.tsx
@@ -0,0 +1,8 @@
+// park list would already be set closest to furthest based on zipcode entered on homepage. (pass zipcode as props)
+// this page would include another zipcode search so the user can look up more than one location.
+// use case (example): if you live in detroit but plan on taking a trip to Arizona.
+// list and define dark park designations (maybe put sources here too)
+
+export default function DarkParkList() {
+    return
+}
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+// current weather conditions bar goes here.
+// maybe logo goes here as well?
+// TO DO: make route to weather api
+
+export default function Header() {
+    return 
+}
