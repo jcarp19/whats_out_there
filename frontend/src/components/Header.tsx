c0240ee75844c295b25ed873c6fdb1d12451a114
rachel-rebecca <Rarebecc@umich.edu>
2021-10-29 16:32:57 -0400

    uploaded logo img, set up header and added background color to body

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,6 +2,12 @@
 // maybe logo goes here as well?
 // TO DO: make route to weather api
 
+import telescope from "../images/telescope.svg"
+
 export default function Header() {
-    return 
+    return (
+        <header>
+           <img src={telescope} alt="icon of telescope" className="logo_pic"/>
+        </header>
+    )
 }
