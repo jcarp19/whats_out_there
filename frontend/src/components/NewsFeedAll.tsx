13c154cc9e0601433c11635abf17948223f20fcc
korchie <korchie44@gmail.com>
2021-11-07 13:01:48 -0500

    starting newsfeed all. not done yet

--- frontend/src/components/NewsFeedAll.tsx
+++ frontend/src/components/NewsFeedAll.tsx
@@ -4,26 +4,69 @@
 
 import WeatherRoute from "../routes/WeatherRoute";
 import { NavLink } from "react-router-dom";
+import NewsInterface, { ArticlesEntity } from "../models/NewsInterface";
+import { useEffect, useState } from "react";
+import { getNews } from "../services/GetNews";
+import DarkParkSearch from "./DarkParkSearch";
+import telescope_Right from "../images/telescope_Right.svg"
+import telescope_Left from "../images/telescope_Left.svg"
 
 // used type "any" to avoid errors, but switch back to "ArticlesEntity" when retrieving all the data.
-export default function NewsFeedAll({ title, author }: any) {
-    return (
-        <div>
-              <nav className="navbar">
-            <ul>
-              <li><NavLink to="/" style={{textDecoration: "none"}}><p className="navbar_p">Home</p></NavLink></li>
-              <li><NavLink to="/learnmore" style={{textDecoration: "none"}}><p className="navbar_p">Learn More</p></NavLink></li>
-              <li><NavLink to="/news" style={{textDecoration: "none"}}><p className="navbar_p">News</p></NavLink></li>
-              <li><NavLink to="/darkparklist" style={{textDecoration: "none"}}><p className="navbar_p">Park List</p></NavLink></li>
-            </ul>
-          </nav>
+export default function NewsFeedAll() {
 
-            <WeatherRoute/>
-            <h1>NewsFeedAll Goes Here!</h1>
-            <p>{title}</p>
-            <p>{author}</p>
-        </div>
-    )
+  const [newsFeedAll, setNewsFeedAll] = useState<NewsInterface>()
+
+  useEffect(() => {
+
+    loadNewsFeedAll();
+  }, [])
+
+  function loadNewsFeedAll() {
+    getNews().then(res => {
+      setNewsFeedAll(res);
+      console.log(res);
+    })
+  }
+
+  return (
+    <div>
+      <nav className="navbar">
+        <ul>
+          <li><NavLink to="/" style={{ textDecoration: "none" }}><p className="navbar_p">Home</p></NavLink></li>
+          <li><NavLink to="/learnmore" style={{ textDecoration: "none" }}><p className="navbar_p">Learn More</p></NavLink></li>
+          <li><NavLink to="/news" style={{ textDecoration: "none" }}><p className="navbar_p">News</p></NavLink></li>
+          <li><NavLink to="/darkparklist" style={{ textDecoration: "none" }}><p className="navbar_p">Park List</p></NavLink></li>
+        </ul>
+      </nav>
+
+      <WeatherRoute />
+      <DarkParkSearch />
+
+      <div>
+        <h2 aria-label="addH2" role="H2" className="park-list-headline">News Feed</h2>
+
+
+        {newsFeedAll?.articles.map((article, index) => {
+          function assignImage(index: any) {
+            if (index % 2 == 0) {
+              let image = telescope_Right;
+              return image;
+            } else {
+              let image = telescope_Left;
+              return image;
+            }
+          }
+
+          return (
+            <div key={index} className="newsArticle_div">
+              <img src={assignImage(index)} alt="telescope inside of a circle" className="telescope_img" />
+              <p>title={article.title} author={article.author} source={article.source} description={article.description} url={article.url} urlToImage={article.urlToImage} publishedAt={article.publishedAt} content={article.content} </p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 
