ad789066ad9d5d558b464596c202109a7973b5b4
korchie <korchie44@gmail.com>
2021-11-12 17:15:38 -0500

    added google auth

---
+++ frontend/src/components/GoogleAuth.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import { signInWithGoogle, signOut } from "../firebaseConfig";
+import googleLogin from "../images/googleLogin.png";
+
+// for logic: if there is a user render this, otherwise render that
+export default function GoogleAuth() {
+    const { user } = useContext(AuthContext);
+
+    return (
+        <div>
+            {!user ? (
+                <img src={googleLogin} onClick={() => {
+                    signInWithGoogle()
+                }} />
+            ) : (
+                <button onClick={() => { signOut() }}>Sign Out</button>
+            )}
+            {/* <p>User Information: </p>
+            <p>{JSON.stringify(user, null, 2)}</p> */}
+        </div>
+    )
+
+
+}
