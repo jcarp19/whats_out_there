5a9e4c1642e205ef6137bdcba6f3c53f2bcdfec3
korchie <korchie44@gmail.com>
2021-11-17 19:45:23 -0500

    saving my changes before I pull

--- backend/functions/src/models/DarkPark.ts
+++ backend/functions/src/models/DarkPark.ts
@@ -1,31 +1,31 @@
-import {ObjectId} from "mongodb";
+import { ObjectId } from "mongodb";
 
 export default interface DarkPark {
-    _id: ObjectId;
-    name: string;
-    title: string;
-    state: string;
-    lightPollution: number;
-    camping: string;
-    fee: string;
-    address: string;
-    latlong: [];
-    url: string;
-    description: string;
-    comments: (Comments)[];
+  _id: ObjectId;
+  name: string;
+  title: string;
+  state: string;
+  lightPollution: number;
+  camping: string;
+  fee: string;
+  address: string;
+  latlong: [];
+  url: string;
+  description: string;
+  comments: Comments[];
 }
 
 export interface Comments {
-    rating: number;
-    comment: string;
-    userName?: string;
-    photoUrl?: string
+  rating: number;
+  comment: string;
+  userName?: string;
+  photoURL?: string;
 }
 
 export interface filteredParks {
-    name: string;
-    state: string;
-    description: string;
-    url: string;
-    miles: number;
+  name: string;
+  state: string;
+  description: string;
+  url: string;
+  miles: number;
 }
--- backend/functions/src/routes/DarkParkRoute.ts
+++ backend/functions/src/routes/DarkParkRoute.ts
@@ -1,44 +1,46 @@
-import express from 'express';
-import { getClient } from '../db';
+import express from "express";
+import { getClient } from "../db";
 import DarkPark from "../models/DarkPark";
 import { Comments } from "../models/DarkPark";
-import { ObjectId } from 'bson';
-
+import { ObjectId } from "bson";
 
 const routes = express.Router();
 
 routes.get("/darkparks", async (req, res) => {
-    try {
-        const client = await getClient();
-        const results = await client.db().collection("darkparks").find().toArray();
-        res.json(results);
-    } catch (err) {
-        console.error("ERROR", err);
-        res.status(500).json({message: "Internal server error"});
-    }
-})
+  try {
+    const client = await getClient();
+    const results = await client.db().collection("darkparks").find().toArray();
+    res.json(results);
+  } catch (err) {
+    console.error("ERROR", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 routes.put(`/darkparks/addcomment/:id`, async (req, res) => {
-    const newComment: Comments = {
-        rating: req.body.rating,
-        comment: req.body.comment,
-        userName: req.body.userName,
-        photoUrl: req.body.photoUrl
-    }
-    const id = req.params.id;
-    // delete newComment.id
+  const newComment: Comments = {
+    rating: req.body.rating,
+    comment: req.body.comment,
+    userName: req.body.userName,
+    photoURL: req.body.photoURL,
+  };
+  const id = req.params.id;
+  // delete newComment.id
 
-    try {
-        const client = await getClient();
-        await client.db()
-        .collection<DarkPark>('darkparks')
-        .updateOne({_id: new ObjectId(id)}, {$push: {comments: newComment}});
-        res.status(200).json(newComment)
-    } catch (err) {
-        console.error("ERROR", err);
-        res.status(500).json({message: "Internal Service Error."})
-    }
-})
-
+  try {
+    const client = await getClient();
+    await client
+      .db()
+      .collection<DarkPark>("darkparks")
+      .updateOne(
+        { _id: new ObjectId(id) },
+        { $push: { comments: newComment } }
+      );
+    res.status(200).json(newComment);
+  } catch (err) {
+    console.error("ERROR", err);
+    res.status(500).json({ message: "Internal Service Error." });
+  }
+});
 
 export default routes;
