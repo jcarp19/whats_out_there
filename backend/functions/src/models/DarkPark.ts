712057f4f5367fa84e5f0f42e6ca5984fbeae9b4
jcarp19 <jim@carpenteronline.us>
2021-11-18 07:23:57 -0500

    db fixes

--- backend/functions/src/models/DarkPark.ts
+++ backend/functions/src/models/DarkPark.ts
@@ -18,8 +18,8 @@ export default interface DarkPark {
 export interface Comments {
     rating: number;
     comment: string;
-    userName?: string;
-    photoURL?: string
+    userName: string;
+    photoURL: string
 }
 
 export interface filteredParks {
